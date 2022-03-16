bcb205391769af710d2d367ed926027dde4102d1
Henrique Vital Carvalho <48650626+henriquevital00@users.noreply.github.com>
2022-03-15 21:46:27 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from prime import *
 from time import perf_counter_ns
 from plot import Plot
 import numpy as np
-import sympy
 
 plot = Plot()
 
-def get_time(data, func=sympy.isprime):
+def get_time(data):
     start = perf_counter_ns()
-    primes = is_prime(data, func)
+    primes = is_prime(data)
     finish = perf_counter_ns()
     return (finish - start)/1000000, primes, finish - start
 
@@ -20,7 +19,6 @@ def get_time_t(data, number_of_threads=5):
 
 def main():
     simple_array = []
-    simple_array_aks = []
     thread_array = []
     thread_20_array = []
     thread_600_array = []
@@ -36,16 +34,16 @@ def main():
 
     print('\n\nanalise de %d valores\n\n'%(len(data)))
 
+    number_t = int(input("Digite o número de threads desejada"))
+
     for i in range(50):
 
         # serial
         tempo_ms_serial, primo_sp, tempo_exec_serial = get_time(data)
-        tempo_ms_serial_aks, primo_sp_aks, tempo_exec_serial_aks = get_time(data, isPrime_aks)
         simple_array.append(tempo_ms_serial)
-        simple_array_aks.append(tempo_exec_serial_aks)
 
         # paralel
-        tempo_ms_threads5, primo_mt, tempo_exec_threads5 = get_time_t(data)
+        tempo_ms_threads5, primo_mt, tempo_exec_threads5 = get_time_t(data, number_t)
         tempo_ms_threads20, primo_mt, tempo_exer_threads20 = get_time_t(data, 20)
         tempo_ms_threads600, primo_mt, tempo_exer_threads600 = get_time_t(data, 600)
 
@@ -61,7 +59,6 @@ def main():
     media_tempo_s = np.mean(simple_array_speedup)
 
     plot.plot_bar(np.mean(simple_array), np.mean(thread_20_array))
-    plot.plot_bar_aks(np.mean(simple_array), np.mean(simple_array_aks))
     plot.plot_threads(np.mean(thread_array), np.mean(thread_20_array), np.mean(thread_600_array), "Tempo X threads", "Media de tempo")
     plot.plot_threads(np.mean(thread_array_speedup), np.mean(thread_20_array_speedup), np.mean(thread_600_array_speedup), "ApeedUp X threads", "SpeedUp")
 
--- prime.py
+++ prime.py
@@ -9,37 +9,11 @@ def check_prime(data):
             prime += 1
     return prime
 
-
-def coef(n, c):
-    c[0] = 1
-    for i in range(n):
-        c[1 + i] = 1
-        for j in range(i, 0, -1):
-            c[j] = c[j - 1] - c[j]
-        c[0] = -c[0]
-
-# function to check whether
-# the number is prime or not
-def isPrime_aks(n):
-    c = [0] * 250000
-    coef(n, c)
-
-    c[0] = c[0] + 1
-    c[n] = c[n] - 1
-
-    i = n
-    while (i > -1 and c[i] % n == 0):
-        i = i - 1
-
-    return True if i < 0 else False
-
-
-
-def is_prime(data, func=sympy.isprime):
+def is_prime(data):
     tamanholista = len(data)
     primos = 0
     for i in range(tamanholista):
-        if func(data[i]):
+        if sympy.isprime(data[i]):
             primos += 1
     return primos
 
